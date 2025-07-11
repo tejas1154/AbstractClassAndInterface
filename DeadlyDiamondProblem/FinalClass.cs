245692b88d1553a7da7553e93898765cabceb078
tejas1154 <tejas.ramane@aurionpro.com>
2025-07-11 09:15:53 +0530

    Add files via upload

---
+++ DeadlyDiamondProblem/FinalClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlyDiamondProblem
+{
+    public class FinalClass : IDerived1, IDerived2
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display method from IBase implemented in FinalClass");
+        }
+    }
+
+}
---
+++ DeadlyDiamondProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DeadlyDiamondProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FinalClass obj  = new FinalClass();
+
+            //No ambiguity
+            obj.Display();
+
+            //Also can cast to other interface
+            IDerived1 d1 = obj;
+            d1.Display();
+
+            IDerived2 d2 = obj;
+            d2.Display();
+
+        }
+    }
+}
